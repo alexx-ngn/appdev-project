f39c68deddef685825f7eccedc77e7ab07f88962
Giddeon1 <143545624+Giddeon1@users.noreply.github.com>
2024-11-27 20:13:35 -0500

    Created Abstract class Account

---
+++ Y/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Y
+{
+    internal abstract class Account
+    {
+        public int Id {  get; set; }
+        public string Name {get; set;}
+        public string email { get; set;}
+        public DateTime creationDate { get; private set; }
+
+        public Account(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            this.email = email;
+            this.creationDate = DateTime.Now;
+        }
+        public abstract void removePost(Post post);
+
+
+    }
+}
