6a5efc9faf254f273ff0676f32003ccb9d13b2bb
Giddeon1 <143545624+Giddeon1@users.noreply.github.com>
2024-11-30 21:39:02 -0500

    finished diagram classes and methods methinks

--- Y/AdminAccount.cs
+++ Y/AdminAccount.cs
@@ -6,17 +6,70 @@ using System.Threading.Tasks;
 
 namespace Y
 {
-    internal class AdminAccount : Account
+    public class AdminAccount : Account
     {
-        private Queue<Report> AssignedReports {  get; set; }
         public AdminAccount(int id, string name, string email) : base(id, name, email)
         {
-            AssignedReports = new Queue<Report>();
         }
 
+        public AdminAccount(string name, string email) : base(name, email)
+        {
+        }
+        public void removeReport(Report ticket)
+        {          
+        }
+
+        public void ban(UserAccount account)
+        {
+            if (ReportSystem.getUserAccount().Contains(account))
+            {
+                ReportSystem.RemoveUser(account);
+            }
+        }
+        public Report fetchNewReport()
+        {
+            Report report = ReportSystem.FetchNextOpenReport();
+            if (report != null)
+            {
+                report.AssignAdmin(this.Id);
+                changeReportStatus(report,Status.Processing);
+                return report;
+            } else
+            {
+                return null;
+            }
+        }
+        public void changeReportStatus(Report report, Status status)
+        {
+            report.ChangeStatus(status);
+        }
+
+        public void closeReport(Report report)
+        {
+            changeReportStatus(report, Status.Closed);
+            ReportSystem.FetchProcessingToClose();
+        }
+
+        public void assignReportTo(Report report, AdminAccount adminAccount)
+        {
+            report.AssignAdmin(adminAccount.Id);
+        }
         public override void removePost(Post post)
         {
-            throw new NotImplementedException();
+            UserAccount user = FindAccountWithPost(post); 
+            user.removePost(post); 
+        }
+
+        private UserAccount FindAccountWithPost(Post post)
+        {
+            foreach (UserAccount user in ReportSystem.getUserAccount()) // Assuming GetUserAccounts fetches all users
+            {
+                if (user.GetPosts().Contains(post)) // Check if the user's posts contain the given post
+                {
+                    return user; // Return the user that owns the post
+                }
+            }
+            return null; // Return null if no user owns the post
         }
     }
 }
---
+++ Y/PostReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Y
+{
+    public class PostReport : Report
+    {
+        private int ReportedPostId { get; set; }
+        public PostReport(int reportingUserId, string reason, int reportedPostId) : base(reportingUserId, reason)
+        {
+            ReportedPostId = reportedPostId;
+        }
+    }
+}
